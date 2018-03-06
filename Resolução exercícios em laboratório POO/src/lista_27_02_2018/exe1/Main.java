a1b68180fb83d473eb6f9886e058f609b4f826fa
aluno <aluno@LAB309.udicentro.local>
2018-03-06 18:40:14 -0300

    ex

--- Resolução exercícios em laboratório POO/src/lista_27_02_2018/exe1/Main.java
+++ Resolução exercícios em laboratório POO/src/lista_27_02_2018/exe1/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		Conta minhaConta = new Conta();
-		minhaConta.numero;
 
 	}
 	// ele nao consegue busca o int numero porque ele está encapsulado na class Conta 
