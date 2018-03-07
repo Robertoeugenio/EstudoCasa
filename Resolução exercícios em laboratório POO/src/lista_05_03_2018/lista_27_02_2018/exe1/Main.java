85ffde63aace5f7cfab07ccc917a252975211180
Roberto Eugenio dos Santos <30935436+Robertoeugenio@users.noreply.github.com>
2018-03-06 22:26:42 -0300

    Add files via upload

---
+++ Resolução exercícios em laboratório POO/src/lista_05_03_2018/lista_27_02_2018/exe1/Main.java
@@ -0,0 +1,11 @@
+package lista_27_02_2018.exe1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Conta minhaConta = new Conta();
+
+	}
+	// ele nao consegue busca o int numero porque ele está encapsulado na class Conta 
+	
+}
